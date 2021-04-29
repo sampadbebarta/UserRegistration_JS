1261af78df4c04142ceeeace2ce6de30b8eea883
Sampad <bebartasampad@gmail.com>
2021-04-29 10:53:02 +0530

    Refactored:Validation and Testing for User Registration Problem

--- src/Main.js
+++ src/Main.js
@@ -8,24 +8,25 @@ let VALIDEMAILPATTERN = new RegExp("^[a-z][a-z0-9]*[.+-]?[a-z0-9]+[@]{1}[a-z0-9]
 let VALIDMOBILEPATTERN = new RegExp("^[0-9]{2}[0-9]{10}");
 let VALIDPASSWORD = new RegExp("^(?=.*[A-Za-z0-9!@#$%^&*])(?=.*[0-9])(?=.*[0-9a-z])(?=.*[A-Z])(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
 
-const user = new UserRegistration();
-
 function checkInput (question,regex) 
 {
-    let input = read.question(question)
-    var result = user.patternMatching(input,regex)
-    while(result == false)
-    {
-        let input = read.question(question)
-        result = user.patternMatching(input,regex)
-        if(result == true)
-        {
-            break;
-        }
-    }
+        return new Promise(function(myResolve, myReject) {
+            let input = read.question(question)
+            var result = UserRegistration.patternMatching(input,regex)
+            while(result == false)
+            {
+                let input = read.question(question)
+                result = UserRegistration.patternMatching(input,regex)
+                if(result == true)
+                {
+                    myResolve("Valid");
+                    break;
+                }
+            }
+    })
 }
 
-checkInput('Enter First Name:',VALIDNAMEPATTERN);
-checkInput('Enter Last Name:',VALIDNAMEPATTERN);
-checkInput('Enter Mobile Number:',VALIDMOBILEPATTERN);
-checkInput('Enter Password:',VALIDPASSWORD);
+
+checkInput('Enter First Name:',VALIDNAMEPATTERN).then(checkInput('Enter Last Name:',VALIDNAMEPATTERN).then(
+    checkInput('Enter Mobile Number:',VALIDMOBILEPATTERN).then(
+        checkInput('Enter Password:',VALIDPASSWORD))))
